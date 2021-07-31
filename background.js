9a60e8e59c75455e76246c312772d92491c35b71
catdogpandas <321259693@qq.com>
2021-07-31 23:12:36 +0800

    删除选项界面和下拉界面

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 let color = "#3aa757";
-browser.runtime.onInstalled.addListener(()=>{
-  browser.storage.sync.set({ color });
+chrome.runtime.onInstalled.addListener(()=>{
+  chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
   
 })
